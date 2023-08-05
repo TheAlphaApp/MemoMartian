c1a5c0fb8c16e7ecf28b54ba38576794fb4a5aaa
TheAlphaApp <souravsuman636@gmail.com>
2023-08-05 09:42:23 +0530

    version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dzdexon.memomartian"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.2"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
