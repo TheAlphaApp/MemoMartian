7cdf80abe7f2458f75287a54ffcb4bf5fab29352
TheAlphaApp <souravsuman636@gmail.com>
2024-10-11 11:53:20 +0530

    changing to New Design

--- app/src/main/java/com/dzdexon/memomartian/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/dzdexon/memomartian/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.dzdexon.memomartian.ui.screens.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -51,7 +50,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
